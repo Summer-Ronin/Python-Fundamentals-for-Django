15cf1b34b790e4f06e9625447726c98d13de5647
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-09-02 16:47:49 +0700

    :zap: udpate to recent work

--- Level_One/Numbers.py
+++ Level_One/Numbers.py
@@ -1,6 +1,47 @@
 """
-    In Python, Numbers have 2 main forms
-    - Integers 
-    - Floating Point
+===================================================
+############## Artithmetic Operators	###########
+===================================================
 """
-# 
+# Addition
+print('5 + 2 = ', 5 + 2)
+
+# Subtraction
+print('5 - 2 = ', 5 - 2)
+
+# Multiplication
+print('5 * 2 = ', 5 * 2)
+
+# Division
+print('5 / 2 = ', 5 / 2)
+
+# Exponent
+print('5 ^ 2 = ', 5 ** 2)
+# This is the same as 5 * 5
+
+# Ordinary Division
+print('5 / 2 = ', 5 / 2)
+
+# Floor Division
+print('5 // 2 = ', 5 // 2, ' (which will get the integer number of the division only)')
+
+# Modulus
+print('5 % 2 = ', 5 % 2)
+
+"""
+===================================================
+############## Assignment #########################
+===================================================
+"""
+# Variables
+firstName = "Théo"
+tax_rate = 0.2
+my_income = 9000
+
+# Assignments
+introduction = "Hi, my name is " + firstName
+actual_tax = tax_rate * my_income
+
+print(introduction, ' and my tax is ', actual_tax)
+print('How about decreasing tax rate: ', --tax_rate)
+
